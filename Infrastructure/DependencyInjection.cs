28fcfacbb94f3ce343f27e27f1c0e62da7f6b147
rzaketovic1 <r.zaketovic@gmail.com>
2026-07-14 17:36:04 +0200

    fix: parse Supabase URI connection string for Render deployment

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -14,8 +14,16 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // Parse if it's a URI format (Supabase/Heroku style)
+        if (connectionString?.StartsWith("postgresql://") == true)
+        {
+            connectionString = ParsePostgresUri(connectionString);
+        }
+
         services.AddDbContext<StoreContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(connectionString));
 
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IUsersRepository, UsersRepository>();
@@ -23,5 +31,26 @@ public static class DependencyInjection
         services.AddScoped<IFileUploadService, FileUploadService>();
 
         return services;
+    }
+
+    private static string ParsePostgresUri(string uri)
+    {
+        try
+        {
+            var parsedUri = new Uri(uri);
+            var userInfo = parsedUri.UserInfo.Split(':');
+            var username = userInfo[0];
+            var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : string.Empty;
+            var host = parsedUri.Host;
+            var port = parsedUri.Port > 0 ? parsedUri.Port : 5432;
+            var database = parsedUri.AbsolutePath.TrimStart('/');
+
+            return $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+        }
+        catch
+        {
+            // If parsing fails, return the original string
+            return uri;
+        }
     }
 }
